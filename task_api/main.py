a632efeeeb2f08161ebe485a4c3f47f17712f7d8
nathanokane80 <nathanokane80@gmail.com>
2025-07-21 16:19:39 +0100

    Began writing a task tracker API

--- task_api/main.py
+++ task_api/main.py
@@ -1,10 +1,20 @@
 from fastapi import FastAPI
 from pydantic import BaseModel
+from typing import Optional
 
 tasks = []
 id = 1
 
 app = FastAPI()
+
+class TaskIn(BaseModel):
+    title: str
+    description : Optional[str] = None
+    completed : bool = False
+
+class TaskUpdate(BaseModel):
+    title : Optional [str] = None
+    completed : Optional [str] = None
 
 
 @app.get("/")
@@ -14,4 +24,50 @@ async def root():
 @app.get("/tasks")
 async def get_tasks():
     return tasks
+
+@app.post("/tasks")
+async def create_task(task: TaskIn):
+    global id
+    new_task = {
+        "id" : id,
+        "title" : task.title,
+        "description" : task.description,
+        "completed" : False
+    }
+    tasks.add(new_task)
+    id += 1
+    return new_task
+
+@app.delete("/tasks/{id}")
+async def delete_task(id : int):
+    for task in tasks:
+        if task["id"] == id:
+            tasks.pop(task)
+            return {"message": "Task deleted"}
+    return {"error": "Task not found"}, 404
+
+
+@app.put("/tasks/{id}")
+async def update_task(id : int, update : TaskUpdate):
+    response = {"messages" : [], "success" : False}
+    for task in tasks:
+        if task["id"] == id:
+            if update.completed is not None:
+                task["completed"] = update.completed
+                response["messages"].append("Task completion status updated")
+            if update.title is not None:
+                task["title"] = update.title
+                response["messages"].append("Task title updated")
+
+            if not response["messages"]:
+                response["messages"].append("No changes made")
+
+            response["success"] = True
+            response["task_id"] = id
+            return response
+
+    response["messages"].append(f"Task with ID {id} not found")
+    return response, 404
+ 
+            
 
